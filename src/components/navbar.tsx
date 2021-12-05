89c70baa99cd31af0a3930a12cc730eac65883c5
Aneta Opletalova <aneta.opletalova@sde.cz>
2021-12-05 15:43:20 +0100

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,44 +1,27 @@
-import React, { useMemo, useState } from 'react';
-import { AppBar, Box, Button, IconButton, Theme, Toolbar, Typography, useTheme } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import './navbar.scss';
+import React, { useMemo } from 'react';
+import { AppBar, IconButton, Theme, Toolbar, Typography, useTheme } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
-import classNames from 'classnames';
-import { theme } from '../theme';
-import { CSSProperties } from '@mui/styled-engine';
+import { AccountCircle } from '@mui/icons-material';
 
 const Navbar = () => {
-    //const classes = useStyles();
-    const [open, setOpen] = useState(false);
     const location = useLocation();
     const theme = useTheme();
     const styles = useMemo(() => createStyles(theme), [theme]);
 
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-    console.log(location.pathname)
-
     return (
-        // <AppBar position='static' color='primary'
-        // className={classNames({
-        //     'hidden-navbar': location.pathname === '/login',
-        // })}>
         <AppBar position='static' color='primary' style={{ display: location.pathname === '/login' ? 'none' : 'block' }}>
-            <Toolbar>
-                <Typography variant="h6" component="div" style={styles.title}>
-                    <Link to="/stats" style={styles.headerLink}>Stats</Link>
-                </Typography>
-                <Typography variant="h6" component="div">
-                    <Link to="/" style={styles.headerLink}>Home</Link>
-                </Typography>
-                {/* <Button variant="outlined" color="inherit">
-                    Button 2
-                </Button> */}
+            <Toolbar style={styles.navbar}>
+                <div style={styles.row as React.CSSProperties}>
+                    <Typography variant="h6" component="div">
+                        <Link to="/" style={styles.headerLink}>Home</Link>
+                    </Typography>
+                    <Typography variant="h6" component="div" style={styles.title}>
+                        <Link to="/stats" style={styles.headerLink}>Statistics</Link>
+                    </Typography>
+                </div>
+                <div>
+                    <IconButton onClick={() => console.log('account')} > <AccountCircle sx={{ color: 'white' }} /> </IconButton>
+                </div>
             </Toolbar>
         </AppBar >
     );
@@ -49,10 +32,6 @@ const createStyles = (theme: Theme) => (
         hiddenNavbar: {
             display: 'none !important',
         },
-        rightMenu: {
-            display: 'flex',
-            justifyContent: 'flex-end',
-        },
         title: {
             marginRight: '15px',
         },
@@ -60,6 +39,14 @@ const createStyles = (theme: Theme) => (
             textDecoration: ' none',
             color: 'white',
             marginRight: '20px',
+        },
+        navbar: {
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+        row: {
+            flexDirection: 'row',
+            display: 'flex',
         }
     }
 );
